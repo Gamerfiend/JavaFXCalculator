f2290eaf41f39c3a6d297c844747444f95eed847
Tyler Bezera <tylerjessilynn@gmail.com>
2018-10-03 13:54:42 -0700

    List of changes: * Extracted Constants * assembleButton function requires DisplayText and an EventHandler * Extracted Grid Pane creation into a function * New addToGridPane method * Basic loop for adding elements onto GridPane to display, this isn't correct yet, just a test loop to get something on the screen.

--- src/ui/CalculatorUI.java
+++ src/ui/CalculatorUI.java
@@ -1,8 +1,14 @@
 package src.ui;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -16,8 +22,16 @@ import javafx.stage.Stage;
  */
 public class CalculatorUI extends Application
 {
-    public static final int CALCULATOR_HEIGHT = 500;
-    public static final int CALCULATOR_WIDTH = 500;
+    public static final int CALCULATOR_HEIGHT = 300;
+    public static final int CALCULATOR_WIDTH = 350;
+
+    public static final int NUM_COLS = 3;
+    public static final int NUM_BOXES = 3;
+
+    public static final int COLUMN_CONSTRAINTS_WIDTH = 40;
+    public static final String[] CalculatorButtonFaces = {"7", "8", "9", "+", "4", "5", "6",
+            "-", "1", "2", "3", "*", "0", "Enter", "/"};
+
     /**
      * The main entry point for all JavaFX applications.
      * The start method is called after the init method has returned,
@@ -44,18 +58,50 @@ public class CalculatorUI extends Application
         primaryStage.show();
     }
 
-    private Button assembleButton()
+    private Button assembleButton(String displayText, EventHandler<ActionEvent> onPressed)
     {
-        return new Button();
+        Button newButton = new Button();
+        newButton.setText(displayText);
+        newButton.setOnAction(onPressed);
+        return newButton;
+    }
+
+    private GridPane assembleGridPane(String styleID, int hGap, int vGap, int insetAmount, int numberColumns, int columnConstraintsWidth)
+    {
+        GridPane gridPane = new GridPane();
+
+        gridPane.setId(styleID);
+
+        gridPane.setHgap(hGap);
+        gridPane.setVgap(vGap);
+
+        gridPane.setPadding(new Insets(insetAmount));
+
+        ObservableList<ColumnConstraints> columnConstraints = gridPane.getColumnConstraints();
+
+        for (int i = 1; i <= numberColumns; i++)
+        {
+            columnConstraints.add(new ColumnConstraints(columnConstraintsWidth));
+        }
+
+        return gridPane;
+    }
+
+    private GridPane addToGridPane(GridPane gridPane, Node itemToAdd, int columnIndex, int rowIndex, int colSpan, int rowSpan)
+    {
+        gridPane.add(itemToAdd, columnIndex, rowIndex, colSpan, rowSpan);
+        return gridPane;
     }
 
     private Scene assembleScene()
     {
-        GridPane gridPane = new GridPane();
-        gridPane.setId("main-grid-pane");
-
-
-
+        GridPane gridPane = assembleGridPane("main-grid-pane", 10, 10, 10, NUM_COLS, COLUMN_CONSTRAINTS_WIDTH);
+        int counter = 0;
+        for(String element : CalculatorButtonFaces)
+        {
+            gridPane = addToGridPane(gridPane, assembleButton(element, null), counter, counter, 1, 1);
+            counter += 1;
+        }
         return new Scene(gridPane, CALCULATOR_WIDTH, CALCULATOR_HEIGHT);
     }
 }
